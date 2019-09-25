873d63cf0155792cceb8cd948cd4b8dba9cdba62
zgx <Administrator@192.168.7.108>
2019-09-25 10:51:53 +0800

    test

--- src/main/java/com/woniuxy/dao/PictureMapper.java
+++ src/main/java/com/woniuxy/dao/PictureMapper.java
@@ -1,9 +1,11 @@
 package com.woniuxy.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.woniuxy.domain.Picture;
 import com.woniuxy.domain.PictureExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface PictureMapper {
 	   
